df4d9ed36aafa9a56d68685db0450887fbb0f7b1
Zhehao Hu <hu_zhehao@hotmail.com>
2024-08-23 12:23:10 +0200

    export function

--- R/zhehaoColor.R
+++ R/zhehaoColor.R
@@ -5,7 +5,7 @@ source('R/palettes.R')
 #' Color palettes extracted from my [photographs](https://zzzhehao.github.io/gallery/gallery.html).
 #' @param palette_id Name of Palette. Complete list of acceptable IDs can be found on Github.
 #' @param n Number of Colors. If not specified, original palette will return, if n exceeds original palette, colors are automatically interpolated.
-
+#' @export
 zhehaoColor <- function(palette_id, n){
   palette <- palettes[[palette_id]]
   pl <- length(palette)
