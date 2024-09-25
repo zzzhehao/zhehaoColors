e1a198eaed0f10f67d1c4eb2db91c1bb89ba2f0e
Zhehao Hu <hu_zhehao@hotmail.com>
2024-09-25 20:57:00 +0200

    add color_calc function

---
+++ R/color_calc.R
@@ -0,0 +1,8 @@
+light_calc <- function(hex, factor) {
+    rgb <- as.vector(col2rgb(hex)[,1]) * (1+factor)
+    if (any(rgb > 255)) {
+        warningCondition("Calculated color exceeds max value (255), set to 255")
+        rgb[rgb > 255] = 255
+        }
+    return(rgb(rgb[1], rgb[2], rgb[3], maxColorValue = 255))
+}
